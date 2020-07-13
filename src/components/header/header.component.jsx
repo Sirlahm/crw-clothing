7f11750da42040e41705e29b336572e00ae9516a
riliwan <riliwanademola@yahoo.com>
2020-07-13 18:15:12 +0100

    added card icon and cart dropdown components to our app ,also implemented card redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import { connect} from  'react-redux'
 import './header.component.scss'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import {auth} from '../../firebase/firebase.utils'
 import {ReactComponent as Logo} from '../../assets/crown.svg'
-
-const Header = ({ currentUser }) => (
+import CartIcon from '../cart-icon/cart-icon.component'
+const Header = ({ currentUser, hidden }) => (
 
     <div className='header '>
      <Link className='logo-container' to='/'>
@@ -20,13 +21,16 @@ const Header = ({ currentUser }) => (
              : 
              (<Link className='option' to='/signin'>SIGN IN</Link>)
          }
-
+        <CartIcon/>
      </div>
+     {hidden ? null : <CartDropdown/>}
+     
     </div>
 )
 
-const mapStateToProps = state => ({
-currentUser : state.user.currentUser
+const mapStateToProps = ({user:{currentUser},cart:{hidden}}) => ({
+currentUser,
+hidden
 
 })
 
